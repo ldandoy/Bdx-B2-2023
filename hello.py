1875d254774919ab6c736f31c3b3b7549b623aca
ldandoy <ldandoy@overconsulting.net>
2023-11-06 15:47:20 +0100

    MAJ

--- hello.py
+++ hello.py
@@ -2,11 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html', name="test")
+
+@app.route("/")
+def hello_world():
+    return render_template(
+        'hello.html',
+        name="Loïc DANDOY"
+    )
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashboard.html')
+
+
+app.run(debug=True, use_reloader=True)

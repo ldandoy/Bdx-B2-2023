c8af0cd5f2fb9532ebac2202a965d72d38985b31
ldandoy <ldandoy@overconsulting.net>
2023-12-04 15:42:48 +0100

    MAJ

---
+++ transform.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from json import loads, dumps
+
+datas = pd.read_csv("data/in/met_agenda.csv",delimiter=";")
+df1 = datas[['Titre','Ville', 'Mots clés', 'Événement physique ou en ligne']]
+
+result = df1.to_json(orient="records")
+parsed = loads(result)
+for key in range(len(parsed)):
+    print(key)
+    result_tmp = parsed[key]['Événement physique ou en ligne']
+    df1.loc[key].at['Événement physique ou en ligne'] = loads(result_tmp)['label']['fr']
+
+df1.to_csv("data/out/import.csv", header=True,index=False)
